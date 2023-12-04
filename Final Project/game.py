25d11aaac05c303e793c115ce536dfaf3884dc77
nicolasekstrand <nicolas.ekstrand@menloschool.org>
2023-12-04 13:43:14 -0800

    Added object orienation and began adding platforms

--- Final Project/game.py
+++ Final Project/game.py
@@ -5,6 +5,13 @@ from player import *
 # Initialize Pygame
 pygame.init()
 
+class Game:
+    def __init__(self, level, width, height):
+        self.level = level
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((self.width, self.height))
+
 # Constants
 WIDTH, HEIGHT = 800, 600
 FPS = 60
@@ -24,6 +31,7 @@ player_velocity_y = 0
 player_velocity_x = 0
 
 hero = Player(50, WIDTH // 2 - player_size // 2, HEIGHT // 2 - player_size // 2, 0, 0)
+game = Game(1, 800, 600)
 
 print(hero)
 
@@ -31,6 +39,32 @@ print(hero)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Low Gravity Platformer")
 clock = pygame.time.Clock()
+
+# Collision detection with platforms
+def checkCollision(platforms):
+    num = 0
+    for platform in platforms:
+        if (
+            hero.get_x() < platform[0] + platform[2]
+            and hero.get_x() + hero.get_size() > platform[0]
+            and hero.get_y() < platform[1] + platform[3]
+            and hero.get_y() + hero.get_size() > platform[1]
+        ):
+            # Collision occurred, stop falling/jumping
+            hero.set_jump(False)
+            hero.set_y_speed(0)
+            num += 1
+            #Last platform in list is objective platform
+            if platform[0] == platforms[-1][0]:
+                game.screen.fill(WHITE)
+                game.level += 1
+                game.width += 100
+                game.height += 100
+                game.screen = pygame.display.set_mode((game.width, game.height))
+                hero.set_y(game.height - 100)
+
+    if num <=0:
+        hero.set_jump(True)
 
 # Main game loop
 while True:
@@ -41,37 +75,41 @@ while True:
 
         # Jump when space is pressed
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            player_velocity_y = JUMP_HEIGHT
+            hero.set_y_speed(JUMP_HEIGHT)
 
     # Handle left and right movement
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        player_velocity_x = -PLAYER_SPEED
+        hero.set_x_speed(-PLAYER_SPEED)
     elif keys[pygame.K_RIGHT]:
-        player_velocity_x = PLAYER_SPEED
+        hero.set_x_speed(PLAYER_SPEED)
     else:
-        player_velocity_x = 0
+        hero.set_x_speed(0)
 
     # Apply gravity
-    player_velocity_y += GRAVITY
-    player_x += player_velocity_x
-    player_y += player_velocity_y
+    hero.increase_y_speed(GRAVITY)
+    hero.move_x()
+    hero.move_y()
 
     # Keep the player within the screen bounds
-    if player_y > HEIGHT - player_size:
-        player_y = HEIGHT - player_size
-        player_velocity_y = 0
+    if hero.get_y() > HEIGHT - hero.get_size():
+        hero.set_y(HEIGHT - hero.get_size())
+        hero.set_y_speed(0)
 
-    if player_x < 0:
-        player_x = 0
-    elif player_x > WIDTH - player_size:
-        player_x = WIDTH - player_size
+    if hero.get_x() < 0:
+        hero.set_x(0)
+    elif hero.get_x() > WIDTH - hero.get_size():
+        hero.set_x(WIDTH - player_size)
 
     # Draw the background
     screen.fill(WHITE)
 
     # Draw the player
     pygame.draw.rect(screen, RED, (hero.get_x(), hero.get_y(), hero.get_size(), hero.get_size()))
+
+    #Draw Rectangles
+    lvl1_rects = [(300, 300, 200, 20)]
+    pygame.draw.rect(screen, (0, 0, 0), (lvl1_rects[0]))
 
     # Update the display
     pygame.display.flip()
--- Final Project/player.py
+++ Final Project/player.py
@@ -21,18 +21,29 @@ class Player:
     def get_y_speed(self):
         return self.y_speed
     
-    def move_hori(self):
+    def move_x(self):
         self.x += self.x_speed
+    
+    def move_y(self):
         self.y += self.y_speed
 
     def set_y_speed(self, x):
         self.y_speed = x
+
+    def set_x_speed(self, x):
+        self.x_speed = x
+
+    def set_x(self, x):
+        self.x = x
 
     def set_y(self, x):
         self.y = x
 
     def increase_y(self, x):
         self.y += x
+
+    def increase_y_speed(self, x):
+        self.y_speed += x
 
     def __str__(self):
         return f"Player - Position: ({self.x}, {self.y}), Velocity: ({self.x_speed}, {self.y_speed}), Size: {self.size}"
